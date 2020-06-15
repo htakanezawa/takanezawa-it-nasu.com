bd850b69550199efdd094a38f4b8e84605dfa022
htakanezawa <takanezawa@it-nasu.com>
2020-06-15 13:36:36 +0900

    説明のコメントを入れました。

--- learning/Delphi/Unit1.pas
+++ learning/Delphi/Unit1.pas
@@ -43,11 +43,12 @@ uses
 
 {$R *.DFM}
 
+//うるう年判断実行のボタンクリック
 procedure TForm1.Button1Click(Sender: TObject);
-var
-  Birthday: TDate;
+var //varブロックでオブジェクトの識別子宣言を行う
+  Birthday: TDate;  //ここではオブジェクトの参照を保存するための識別子を宣言したのみ
 begin
-  Birthday := TDate.Create;
+  Birthday := TDate.Create;  //これでオブジェクトの実データ（インスタンス）を確保して、その参照をオブジェクト識別子に代入
 //  Birthday.SetValue(2020, 2, 14);
   try
     if (Length(Trim(Year.Text)) = 0) or
@@ -64,49 +65,52 @@ begin
     else
       ShowMessage('うるう年ではない');
   finally
-    Birthday.Free;
+    Birthday.Free;  //これでオブジェクトのために確保したメモリを解放
   end;
 end;
 
+//親クラスと子クラスの処理の違いを見るための実行ボタン
 procedure TForm1.Button2Click(Sender: TObject);
 var
-  myDay: TDate;
-  myNewDay: TNewDate;
-  myObject: TObject;
+  myDay: TDate;        //親クラスのオブジェクト識別子
+  myNewDay: TNewDate;  //子クラスのオブジェクト識別子
+  myObject: TObject;   //TObject型のオブジェクト識別子を宣言
 begin
-  if Sender is TButton then
-    show(TButton(Sender).Caption);
+  if Sender is TButton then         //Senderオブジェクト識別子に入っているオブジェクトが TButtonか判定
+    show(TButton(Sender).Caption);  //SenderをTButton型にキャストし、TButton型のTextプロパティを使用
 
-  myDay := TDate.Create;
+  myDay := TDate.Create;   //親オブジェクトのCreate
   myObject := myDay;
-  myNewDay := TNewDate.Create;
+  myNewDay := TNewDate.Create; //子オブジェクトのCreate
   try
-    show('親クラス：'+ myday.GetText);
-    show('子クラス：'+ myNewDay.GetText);
+    show('親クラス：'+ myday.GetText);     //親オブジェクトのGetTextメソッドで文字列を取得して表示
+    show('子クラス：'+ myNewDay.GetText);  //子オブジェクトのGetTextメソッドで文字列を取得して表示
 
-    show((myObject as TDate).Gettext);
+    show((myObject as TDate).Gettext);  //myObjectをTDate型のオブジェクトとして、TdateのメソッドGetTextを使用
 
   finally
-    myDay.Free;
-    myNewDay.Free;
+    myDay.Free;      //親オブジェクトの解放
+    myNewDay.Free;   //子オブジェクトの解放
   end;
 end;
 
+//動物の鳴き声を表示させる
 procedure TForm1.Button3Click(Sender: TObject);
 var
   myAnimal: TKemono;
 begin
-  if rbServal.Checked then
-    myAnimal := TServal.Create
+  if rbServal.Checked then       // 選ばれているチェックボックスによって TServalかTFennecクラスのどちらかをCreate
+    myAnimal := TServal.Create   // TServalのオブジェクト参照を親クラス型のオブジェクト識別子に代入
   else
-    myAnimal := TFennec.Create;
+    myAnimal := TFennec.Create;  // TFennecのオブジェクト参照を親クラス型のオブジェクト識別子に代入
   try
-    show(myAnimal.Voice);
+    show(myAnimal.Voice);        //選択された動物の鳴き声を表示
   finally
     myAnimal.Free;
   end;
 end;
 
+//クラス型名を渡して動物の鳴き声を表示させる
 procedure TForm1.Button4Click(Sender: TObject);
 begin
   if rbServal.Checked then
--- learning/Delphi/Unit2.pas
+++ learning/Delphi/Unit2.pas
@@ -5,139 +5,169 @@ interface
 uses
   SysUtils, DateUtils;
 
-type
-  TDate = Class
-    private
-      FDate: TDateTime;
+//https://qiita.com/kazaiso/items/b3941a234853b9cb6abb
+//でクラスの基本的な概要を学習し、例を見ながらコードを記述しました。
+//https://qiita.com/kazaiso/items/c76eac60bdd4dd075fa3
+//でprivate, public , protected　隠蔽と公開、コンストラクタ, デストラクタについて学習しながら
+//ソースコートを変更しました。
+//https://qiita.com/kazaiso/items/5d6b58966a00e91ae198
+//でクラスの継承について学習しながら
+//ソースコートを変更しました。
 
-    public
-      constructor Create; overload;
-      constructor Create(Y, M, D: Integer); overload;
+type //typeブロックでクラス定義を行う
+  TDate = Class   //Tdateというクラス定義識別子を、「 = class 」としてクラス定義 class(TObject)と同じ
+    private  //他ユニットからは参照できず、アクセスもできない
+             //将来的なコードの改変時において、直接アクセスされていない内部フィールドを変更したとしても、コード改変の影響が少ない
+      //旧実装 Year, Month, Day: Integer;
+      FDate: TDateTime;   //日付を保持する TDateTime型 フィールド
 
-      Procedure SetValue(Y, M, D: Integer); overload;
-      Procedure SetValue(newDate: TDateTime); overload;
+    public   //他ユニットから参照、アクセス可能
+      //クラスに紐づいたメソッド
+      constructor Create; overload;      //デフォルトのCreateと同じ記述でOverload指定
+      constructor Create(Y, M, D: Integer); overload;  //初期値を与えるCreateのoverload
 
-      Function UruuDoshi: Boolean;
-      Function GetText: String;
+      Procedure SetValue(Y, M, D: Integer); overload;    //年月日をIntegerでセットするメソッド
+      Procedure SetValue(newDate: TDateTime); overload;  //年月日をTDateTimeでセットするメソッド
+
+      Function UruuDoshi: Boolean;    //うるう年かチェックするメソッド
+      Function GetText: String;       //他ユニットからは直接フィールドにアクセスできないので、日付を文字列で取得する関数追加
+
+    Protected   //他ユニットからは使用できない、当クラスを継承するクラスで参照・アクセス可能
   end;
 
 type
-  TNewDate = class(TDate)
+  TNewDate = class(TDate)   //TDateから派生（TDateを継承）する 子クラス（サブクラス）
     public
-      function GetText: String;
+      function GetText: String;   //親の持つ GetTextメソッドをTNewDateクラス版に書き換えるための宣言
   end;
 
 
 type
-  TPerson = class
+  TPerson = class  //新たにTPersonクラスを定義
     private
       FName: String;
-      FBirthDay: TDate;
+      FBirthDay: TDate;  //以前に定義した日付情報をもつTDateクラスをフィールドとして使用
     public
-      constructor Create(name: String);
-      destructor Destroy; override;
+      //https://qiita.com/kazaiso/items/c76eac60bdd4dd075fa3
+      constructor Create(name: String);  //コンストラクター Create の宣言
+      destructor Destroy; override;      //デストラクタ Destroyの宣言 + override
   end;
 
 type
-  TKemono = class(TObject)
+  // TServal、TFennec の親クラスとなるTKemonoクラス
+  TKemono = class(TObject)   // TServel, TFennecの親クラス
     public
+      // Voice 文字列を返すメソッド。実装は子に任せるためvirtual; abstract; キーワードをつけている
       function Voice: string; virtual; abstract;
   end;
 
+  // TKemono の子クラス
   TServal = class(TKemono)
     public
+      // 親クラス内のVoice 文字列を返すメソッド abstractのVoiceメソッド overrideするキーワード付き
       function Voice: string; override;
   end;
 
+  // TKemono の子クラス
   TFennec = class(TKemono)
     public
+      // 親クラス内のVoice 文字列を返すメソッド abstractのVoiceメソッド overrideするキーワード付き
       function Voice: string; override;
   end;
 
-  TFriends = class of TKemono;
+  TFriends = class of TKemono;  // TKemonoクラスの型名を表す TFriends を定義
 
+  // 渡されたパラメータによりオブジェクトをCreateする
   function KemonoVoice(KemonoClassName: TFriends): String;
 
 implementation
 
 
-Procedure TDate.SetValue(Y, M, D: Integer);
+Procedure TDate.SetValue(Y, M, D: Integer); //フィールドTDateTime化に伴い実装変更
 begin
-  FDate := EncodeDate(Y,M,D);
+  //旧実装  Year := Y; Month := M; Day := D;
+  FDate := EncodeDate(Y,M,D);    //FDateフィールドに代入するように変更
 end;
 
-procedure TDate.SetValue(newDate: TDateTime);
+procedure TDate.SetValue(newDate: TDateTime);  //TDateTime型のパラメータの時実行.実装部にはoverloadキーワード不要
 begin
   FDate := newDate;
 end;
 
-Function TDate.UruuDoshi: Boolean;
+Function TDate.UruuDoshi: Boolean;   //フィールド変更に伴い実装変更
 begin
-  Result := IsLeapYear(YearOf(Fdate));
+  //旧実装 Result := IsLeapYear(Year);
+  Result := IsLeapYear(YearOf(Fdate));   //FDateから年情報を取り出してチェック
 end;
 
-constructor TDate.Create;
+constructor TDate.Create;   //実装部においてoverloadキーワードは記述不要
 begin
-  inherited;
-  FDate := Date;
+  inherited;   //メソッド名もパラメータも同じであれば inherited の後ろのパラメータは省略可能
+  FDate := Date;  //初期値としてDateで今日の日付を代入
 end;
 
-constructor TDate.Create(Y, M, D: Integer);
+//チュートリアルに説明だけはあるが、このチュートリアルの中では実際には使われていない。
+constructor TDate.Create(Y, M, D: Integer);  //初期値有のCreate.実装部においてoverloadキーワードは記述不要
 begin
-  inherited Create;
-  FDate := EncodeDate(Y, M, D);
+  inherited Create;  //パラメータが親クラスのCreateと異なるので、省略できず、inheritedの後ろにCreateを記述して親のCreateを実行することを明記している
+  FDate := EncodeDate(Y, M, D); //指定されたパラメータをTDateTime型に変換して初期値として代入
 end;
 
-function TDate.GetText: String;
+function TDate.GetText: String;     //フィールドにアクセス制限があるため、必要情報を提供する新規関数
 begin
   Result := DateToStr(FDate);
 end;
 
-constructor TPerson.Create(name: String);
-begin
-  inherited Create;
-  FName := name;
-  FBirthDay := TDate.Create;
+//チュートリアルに説明だけはあるが、このチュートリアルの中では実際には使われていない。
+constructor TPerson.Create(name: String);   //コンストラクター Create の実装
+                                            //フィールドに特別な値を、初期値として入れたい場合はここで行う
+begin                                       //フィールドにクラスを使いたい場合には、ここでCreateしてオブジェクトを確保しておく
+  inherited Create;   //パラメータなしの親クラスのCreateを実行する
+  FName := name;      //Create時の初期値としてフィールドに代入
+  FBirthDay := TDate.Create;   //フィールドに使用しているTDate型のクラスの確保
 end;
 
-destructor TPerson.Destroy;
+destructor TPerson.Destroy;   //デストラクタ Destroyの宣言 (実装部に override キーワードは不要）
 begin
-  FBirthDay.Free;
-  inherited;
+  FBirthDay.Free;     //Create時に確保したFBirthDayの破棄
+  inherited;          //何もついていないinheritedは、実装のメソッド名と全く同じメソッド名＆パラメータの親メソッドを実行する
 end;
 
 { TNewDate }
 
-function TNewDate.GetText: String;
+function TNewDate.GetText: String;   //TNewDateクラスで親と同名のGetTextメソッドを新たに記述
+                                     //ここで TNewDateクラスに定義されていないFDateフィールドが使えるのは
+                                     //親クラスのTDateクラスで定義されている為(子クラスで再定義しなければ
+                                     //親クラスのフィールド、メッソッドをそのまま使うことができる）
 begin
   Result := FormatDateTime('gge年m月d日(dddd)', FDate);
 end;
 
 { TServal }
 
-function TServal.Voice: string;
+function TServal.Voice: string;  //TServalのVoiceメソッド実装。実装部にはoverrideキーワードは不必要
 begin
   Result := 'meow';
 end;
 
 { TFennec }
 
-function TFennec.Voice: string;
+function TFennec.Voice: string; //TFenncのVoiceメソッド実装。実装部にはoverrideキーワードは不必要
 begin
   Result := 'yelp';
 end;
 
+//クラス型名により動物の鳴き声を返す
 function KemonoVoice(KemonoClassName: TFriends): String;
 var
-  myAnimal: TKemono;
+  myAnimal: TKemono;  //親クラスとなるTkemonoのオブジェクト識別子を宣言
 begin
-  myAnimal := KemonoClassName.Create;
+  myAnimal := KemonoClassName.Create;   //パラメータで渡されたクラスの型（TServalかTFennec）をCreate
   try
-    Result := myAnimal.Voice;
+    Result := myAnimal.Voice;           //パラメータで渡されたクラス型でCreateしたオブジェクトのクラスメソッドを使用
   finally
     myAnimal.Free;
   end;
 end;
 
 end.
- 
